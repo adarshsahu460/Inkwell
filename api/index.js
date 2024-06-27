173654fa3335539687251a0c1095ca9ed16425c8
AdarshSahu <100473287+adarshsahu460@users.noreply.github.com>
2024-06-27 16:30:54 +0530

    Second Commit

---
+++ api/index.js
@@ -0,0 +1,167 @@
+const { PrismaClient } = require('@prisma/client')
+const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+const app = express();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
+const salt = bcrypt.genSaltSync(10);
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
+
+app.use(cors({credentials:true,origin:'http://localhost:5173'}));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+const prisma = new PrismaClient()  
+
+app.post('/register', async (req,res) => {
+  const {username,password} = req.body;
+  try{
+    const userDoc = await prisma.user.create({
+      data:{
+        username,
+        password:bcrypt.hashSync(password,salt),
+      }
+    })
+    res.json(userDoc);
+  } catch(e) {
+    console.log(e);
+    res.status(400).json(e);
+  }
+});
+
+app.post('/login', async (req,res) => {
+  const {username,password} = req.body;
+  const userDoc = await prisma.user.findFirst({
+    where:{
+      username
+    }
+  })
+  const passOk = bcrypt.compareSync(password, userDoc.password);
+  if (passOk) {
+    jwt.sign({username,id:userDoc.id}, secret, {}, (err,token) => {
+      if (err) throw err;
+      res.cookie('token', token).json({
+        id:userDoc.id,
+        username,
+      });
+    });
+  } else {
+    res.status(400).json('wrong credentials');
+  }
+});
+
+app.get('/profile', (req,res) => {
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, (err,info) => {
+    if (err) throw err;
+    res.json(info);
+  });
+});
+
+app.post('/logout', (req,res) => {
+  res.cookie('token', '').json('ok');
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req,res) => {
+  const {originalname,path} = req.file;
+  const parts = originalname.split('.');
+  const ext = parts[parts.length - 1];
+  const newPath = path+'.'+ext;
+  fs.renameSync(path, newPath);
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const user = await prisma.user.findFirst({
+      where:{
+        username:info.username
+      }
+    })
+    const {title,summary,content} = req.body;
+    try{
+      const postDoc = await prisma.post.create({
+        data:{
+          title,
+          summary,
+          content,
+          cover:newPath,
+          user:{connect:{
+            id:user.id
+          }}
+        }
+      });
+      res.json(postDoc);
+    }catch(err){
+      console.log(err)
+      console.log(JSON.stringify(info))
+    }
+  });
+
+});
+
+app.put('/post',uploadMiddleware.single('file'), async (req,res) => {
+  let newPath = null;
+  if (req.file) {
+    const {originalname,path} = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    newPath = path+'.'+ext;
+    fs.renameSync(path, newPath);
+  }
+
+  const {token} = req.cookies;
+  jwt.verify(token, secret, {}, async (err,info) => {
+    if (err) throw err;
+    const {id,title,summary,content} = req.body;
+    const postDoc = await prisma.post.findFirst({
+      where:{
+        id
+      }
+    })
+    const isAuthor = JSON.stringify(postDoc.userId) === JSON.stringify(info.id);
+    if (!isAuthor) {
+      return res.status(400).json('you are not the author');
+    }
+    await prisma.post.update({
+      where:{
+        id
+      },
+      data:{
+        title,
+        summary,
+        content,
+        cover: newPath ? newPath : postDoc.cover,
+      }
+    })
+    res.json(postDoc);
+  });
+
+});
+
+app.get('/post', async (req,res) => {
+  const posts = await prisma.post.findMany({
+    include:{
+      user:true
+    }
+  });
+  res.json({
+      posts
+  });
+});
+
+app.get('/post/:id', async (req, res) => {
+  const {id} = req.params;
+  const postDoc = await prisma.post.findFirst({
+    where:{
+      id
+    }
+  })
+  res.json(postDoc);
+})
+
+app.listen(4000);
